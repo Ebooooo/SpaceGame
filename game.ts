f40f3fde23809705c765f553c99a1183c46362ce
kamilpajor <48621302+kamilpajor@users.noreply.github.com>
2020-07-07 22:34:05 +0200

    Add files via upload

--- game.ts
+++ game.ts
@@ -1,40 +1,66 @@
-class Obstacle {
-  public positionX = 0;
-  public positionY = 0;
-  private speed = 4;
-  public shape = [];
+class Space {
+  private canvas : HTMLCanvasElement;
+  private ctx : CanvasRenderingContext2D;
 
-  constructor(initialX, initialY, speed = 4) {
-    this.positionX = initialX;
-    this.positionY = initialY;
-    this.speed = speed;
+  private playerX = 0;
+  private playerY = 0;
 
-    const shapeSides = Math.random() * 5;
+  private obstacles = new Array<Obstacle>();
 
-    for (let i = 0; i < shapeSides; i++) {
-      this.shape.push({
-        x: -8 + Math.random() * -8,
-        y: 8 + Math.random() * 8,
-      });
+  private level = 0;
+  private score = 0;
+  private bestScore = 0;
 
-      this.shape.push({
-        x: 8 + Math.random() * 8,
-        y: 8 + Math.random() * 8
-      });
+  private bgRed = 8;
+  private bgGreen = 8;
+  private bgBlue = 72;
 
-      this.shape.push({
-        x: 8 + Math.random() * 8,
-        y: -8 + Math.random() * -8,
-      });
+  constructor(selector) {
+    this.canvas = document.querySelector(selector) as HTMLCanvasElement;
+    this.ctx = this.canvas.getContext('2d');
 
-      this.shape.push({
-        x: -8 + Math.random() * -8,
-        y: -8 + Math.random() * -8,
-      });
+    this.update = this.update.bind(this);
+    this.input = this.input.bind(this);
+    this.moveObstacles = this.moveObstacles.bind(this);
+    this.detectCollision = this.detectCollision.bind(this);
+
+    this.playerX = this.canvas.width / 2;
+    this.playerY = this.canvas.height - 32;
+
+    this.canvas.addEventListener('mousemove', this.input);
+    this.update();
+  }
+
+  private update() {
+    this.render();
+    this.moveObstacles();
+    this.detectCollision();
+
+    requestAnimationFrame(this.update);
+  }
+
+  private render() {
+    this.ctx.fillStyle = `rgba(${this.bgRed}, ${this.bgGreen}, ${this.bgBlue})`;
+    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.width);
+
+    this.ctx.fillStyle = '#fff';
+    this.ctx.beginPath();
+    this.ctx.moveTo(this.playerX + 12.5,this.playerY);
+    this.ctx.lineTo(this.playerX,this.playerY + 25);
+    this.ctx.lineTo(this.playerX + 25,this.playerY + 25);
+    this.ctx.fill();
+
+    for (const obstacle of this.obstacles) {
+
+      this.ctx.beginPath();
+      this.ctx.moveTo(obstacle.positionX, obstacle.positionY);
+      for (const shape of obstacle.shape) {
+        this.ctx.lineTo(obstacle.positionX + shape.x,obstacle.positionY + shape.y);
+      }
+      this.ctx.fill();
     }
-  }
 
-  public moveDown() {
-    this.positionY = this.positionY + this.speed;
+    this.ctx.font = 'bold 16px monospace';
+    this.ctx.fillStyle = "#ffffff";
+    this.ctx.fillText(`LEVEL: ${this.level + 1}, SCORE: ${this.score}, BEST: ${this.bestScore}`, 10, 20);
   }
-}
